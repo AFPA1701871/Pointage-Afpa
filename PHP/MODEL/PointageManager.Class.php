149cdd308d60e256c16b7e12f7552f7512b7cc93
afpa1701871 <martine.poix@afpa.fr>
2020-04-06 10:18:33 +0200

    class + manager Loic

--- PHP/MODEL/PointageManager.Class.php
+++ PHP/MODEL/PointageManager.Class.php
@@ -58,5 +58,32 @@ class PointageManager
         }
         return $caisse;
     }
+        
+    /**
+     * majPointage : met à jour le pointage d'un stagiaire pour une semaine donnée
+     *
+     * @param  mixed $idSemaine             semaine a traiter
+     * @param  mixed $idStagiaire           stagiaire a traiter
+     * @param  mixed $tabPointage           tableau à 2 dimensions et 9 cases, la premiere contient le code pointage, la 2de le commentaire
+     * @return void
+     */
+    public static function majPointage($idSemaine, $idStagiaire,$tabPointage)
+    {
+        $db = DbConnect::getDb();
+        $lesJours = JourneeManager::findById($idSemaine);
+        $q = $db->prepare("INSERT INTO pointage (idStagiaire, idJournee, idPresence, commentaire, validation) VALUES (:idStagiaire, :idJournee, :idPresence, :commentaire, :validation)");
+        $q->bindValue(":idStagiaire", $obj->getIdStagiaire());
+        $q->bindValue(":idJournee", $obj-> getIdJournee());
+        $q->bindValue(":idPresence", $obj->getIdPresence());
+        $q->bindValue(":commentaire", $obj->getCommentaire());
+        $q->bindValue(":validation", $obj->getValidation()); 
+        $q->execute();
+        while ($donnees = $q->fetch(PDO::FETCH_ASSOC)) {
+            if ($donnees != false) {
+                $caisse[] = new Pointage($donnees);
+            }
+        }
+        return $caisse;
+    }
 
 }
