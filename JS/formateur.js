7e859bb1e6d26ef96aeff193f31f5a9dfa996e39
afpa1701871 <martine.poix@afpa.fr>
2020-04-09 18:34:02 +0200

    js formateur

--- JS/formateur.js
+++ JS/formateur.js
@@ -1,7 +1,8 @@
-var listSelect = document.querySelectorAll("select"); // on liste tous les SELECT et si il n'est pas rempli, sa bordure devient rouge
-var listCheckbox = document.querySelectorAll('input[type=checkbox]'); // on liste toutes les CHECKBOX 
-var listMessages = document.querySelectorAll(".messageCheck"); // on liste aussi les MESSAGES
-
+var listSelect = document.querySelectorAll("select"); // on liste tous les SELECT
+var listCheckbox = document.querySelectorAll('input[type=checkbox]'); // on liste les CHECKBOX 
+var listMessages = document.querySelectorAll(".messageCheck"); // on liste les MESSAGES
+var listStagiaire = document.querySelectorAll('[id^="idStagiaire"]'); // on liste les STAGIAIRES
+console.log(listStagiaire)
 // ============================================================================================= SELECT PRESENCE (FORMATEUR)
 for (let i = 0; i < listSelect.length; i++) {
 
@@ -26,6 +27,7 @@ function redBox() {
 for (let i = 0; i < listCheckbox.length; i++) { // et on ajoute un listener qui appelle la fonction CHECKBOX
     listCheckbox[i].addEventListener("input", function() {
         checkBox(i)
+        checkSelect();
     });
 }
 
@@ -37,5 +39,56 @@ function checkBox(id) {
     else {
         listMessages[id].innerHTML = "";
     }
+}
+
+
+// ============================================================================================= SELECT ET INPUT (FORMATEUR)
+// Fonction : si la case est cochée, tous les SELECT deviennent des INPUT
+function checkSelect() {
+
+    if (listCheckbox[4].checked == true) { // vendredi
+        var listCombo = document.querySelectorAll('[id^="combo8"') // on liste les combobox du vendredi
+        for (let j = 0; j < listStagiaire.length; j++) {
+                selectToInput(listCombo[j].id);
+        }
+    }
+    if (listCheckbox[4].checked == false) {
+        var listCombo = document.querySelectorAll('[id^="combo8"') // on liste les combobox du vendredi
+        for (let j = 0; j < listStagiaire.length; j++) {
+                inputToSelect(listCombo[j].id);
+        }
+    }
+    else if (listCheckbox[3].checked == true) { // jeudi
+    }
+    else if (listCheckbox[2].checked == true) { // mercredi
+    }
+    else if (listCheckbox[1].checked == true) { // mardi
+    }
+    else if (listCheckbox[0].checked == true) { // lundi
+    }
+}
+
+function selectToInput(idSelect) {
+    var select = document.getElementById(idSelect); // le select
+    var value = select.options[select.selectedIndex].text;  // la valeur sélectionné du select
+
+    var parent = select.parentNode; // la div parent
+
+    select.id = "old"+idSelect;
+    select.style.display = "none"; // on cache le select
+
+    if (document.getElementById(idSelect) == undefined)
+        parent.innerHTML += '<input readonly id="'+idSelect+'" name="'+idSelect+'" value="'+value+'">'; // et on insère un input à la place
+}
+
+
+function inputToSelect(idInput) {
+    var input = document.getElementById(idInput); // l'input
+    var parent = input.parentNode; // la div parent
+    var select = parent.firstChild; // on cherche le select
+
+    parent.removeChild(input);
+    select.id = select.id.substring(3, select.id.length);
+    select.style.display = "block";
 }
 
